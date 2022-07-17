927d3aa9cbefdf7485432cb731173bc2ee99bc3a
Dan Coffey <dcoffey90@gmail.com>
2022-07-17 22:48:49 +0100

    New button for upcoming movies page.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToMustWatchIcon from '../components/cardIcons/addToMustWatch.js'
 
 const UpcomingMoviesPage = (props) => {
   const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
@@ -24,7 +24,7 @@ const UpcomingMoviesPage = (props) => {
       title='Upcoming Movies'
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <AddToMustWatchIcon movie={movie} />
       }}
     />
   );
